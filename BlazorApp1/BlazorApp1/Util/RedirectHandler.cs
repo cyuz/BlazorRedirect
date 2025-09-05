880923b66c7d977f54c29fd4bd77c8ceb54bec12
yuchih_chen <A0244@ose.com.tw>
2025-09-05 14:30:31 +0800

    Upload

---
+++ BlazorApp1/BlazorApp1/Util/RedirectHandler.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+
+namespace BlazorApp1.Util
+{
+    public static class RedirectHandler
+    {
+        public static async Task<bool> VeirfySholudRedirectAsync(ProtectedSessionStorage protectedSessionStore)
+        {
+            var result = await protectedSessionStore.GetAsync<string>("FirstLogin");
+            bool isFirstLogin = !result.Success || string.IsNullOrEmpty(result.Value);
+
+            if (!isFirstLogin)
+            {
+                return false;
+            }
+
+            await protectedSessionStore.SetAsync("FirstLogin", false);
+
+            return true;
+        }
+
+        public static async Task DeleteFlagAsync(ProtectedSessionStorage protectedSessionStore)
+        {
+            await protectedSessionStore.DeleteAsync("FirstLogin");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # BlazorRedirect
 Minimal example for Blazor redirect problem
+
+The system route user to welcome page by checking flag.
+
+modify App.razor
+- use RoutesWithOnNaviagte, will see some flash.
+- use RoutesWithLocationChanging, not work at first open url with browser.
+- use RoutesWorkaround, final solution but complex.
